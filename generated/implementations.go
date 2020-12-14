78e70e5f6a60f5fd90a0792266247537e329ca29
Kasey Kirkham <kasey@upbound.io>
2020-12-14 12:56:11 -0600

    removing now-unused overlay, spruce up iam_user import

--- generated/implementations.go
+++ generated/implementations.go
@@ -1,12 +1,11 @@
 package generated
 
 import (
-	iam "github.com/crossplane-contrib/provider-terraform-aws/generated/resources/iam_user/v1alpha1"
+	iam_user "github.com/crossplane-contrib/provider-terraform-aws/generated/resources/iam_user/v1alpha1"
 	"github.com/crossplane-contrib/terraform-runtime/pkg/plugin"
 )
 
 // TODO: output this list somewhere in the codegen pipeline
 var ResourceImplementations = []*plugin.Implementation{
-	//iam.ServiceAccountImplementation(),
-	iam.Implementation(),
+	iam_user.Implementation(),
 }
